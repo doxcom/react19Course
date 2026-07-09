0c06fe1f119ad0ae28ce0cd6980ab8e754cae0c3
doxcom <aldoperez1319@gmail.com>
2026-07-09 00:31:11 -0600

    adding style into tag selected, and fixing to do, doing, and done icon title

--- tasktrek/tasktresk/src/components/TaskColumn.jsx
+++ tasktrek/tasktresk/src/components/TaskColumn.jsx
@@ -4,12 +4,12 @@ import Todo from "../assets/direct-hit.png"
 import "./TaskColumn.css"
 import TaskCard from './TaskCard'
 
-const TaskColumn = (icon,title) => {
+const TaskColumn = (props) => {
   return (
     <section className='task_column'>
        <h2 className="task_column_heading">
-        <img  className="task_column_icon" src={icon} 
-        alt='' /> {title}
+        <img  className="task_column_icon" src={props.icon} 
+        alt='' />{props.title}
         </h2>
         <TaskCard />
     </section>
--- tasktrek/tasktresk/src/components/TaskForm.jsx
+++ tasktrek/tasktresk/src/components/TaskForm.jsx
@@ -3,32 +3,50 @@ import React, {useState} from 'react'
 import "./TaskForm.css";
 import Tag from './Tag';
 
-const TaskForm = () => {
-      const [taskDate, setTaskData] = useState({
+     const TaskForm = () => {
+      
+      const [taskData,setTaskData] = useState({
         task: "",
-        status: "todo"
-      })
-
-      const handleChange = () =>{
-
+        status: "todo",
+        tags: []
+      });
+     
+      const checkTag = (tag) => {
+        return taskData.tags.some(item => item === tag)
+      }
+      
+      const selectTag = (tag) => {
+           if(taskData.tags.some(item => item === tag)){
+          const filterTags =  taskData.tags.filter(item => item !== tag)
+          setTaskData(prev =>{
+            return {...prev, tags: filterTags}
+          })
+        }else{
+          setTaskData(prev =>{
+            return {...prev, tags: [...prev.tags, tag]}
+          })
+        }
+      };
+       
+      const handleChange = (e) =>{
+        //main logic here, the e is event object for fields
+        const {name, value} = e.target; // "name"- input of enter your task value
+                                       //"value" - drop down value
+        setTaskData(prev =>{
+          return {...prev,[name]: value}
+        })
       }
 
-  //const [task, setTask] = useState("");
-  //const [status, setStatus] = useState("todo");
-
-  //const handleTaskChange = (e) =>{
-  //  setTask(e.target.value);
- // };
-  
-   //const handleStatusChange = (e) =>{
-   // setStatus(e.target.value);
-  //};
+const handleSubmit = (e) =>{
+      e.preventDefault();
+}      
 
   return (
     <header className='app_header'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
         type="text" 
+        name="task"
         className="task_input"
         placeholder='Enter your task'
         onChange={handleChange}
@@ -36,14 +54,16 @@ const TaskForm = () => {
 
         <div  className='task_form_bottom_line'>
           <div>
-            <Tag tagName='HTML'/>
-            <Tag tagName='CSS'/>
-            <Tag tagName='JavaScript'/>
-            <Tag tagName='React'/>
+            <Tag tagName='HTML' selectTag={selectTag} selected={checkTag("HTML")}/>
+            <Tag tagName='CSS' selectTag={selectTag} selected={checkTag("CSS")}/>
+            <Tag tagName='JavaScript' selectTag={selectTag} selected={checkTag("JavaScript")}/>
+            <Tag tagName='React' selectTag={selectTag} selected={checkTag("React")}/>
           </div>
 
           <div>
-            <select className='task_status' 
+            <select 
+              name="status"
+              className='task_status' 
               onChange={handleChange}>
               <option value="todo"> To do</option>
               <option value="doing"> Doing</option>
